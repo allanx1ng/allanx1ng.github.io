735b077f83ed4ed26510daaabeceb5c2b447bef2
allanx1ing <allanxing2003@gmail.com>
2022-09-22 21:48:14 -0700

    contact form styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,35 @@
 import '../App.css';
 
 export default function Contact() {
+
+    const submit = () => {
+        console.log('submitted')
+    }
+
     return (
         <div className='contact' id='contact'>
             <h1>
-                Coming Soon!
+                {"Contact - (not functional yet)"}
             </h1>
             <div>
-                
+                <form className='contact-form'>
+                    <a name="contact"></a>
+
+                    <label>Name</label>
+                    <input class="input-field" type="text" name="name" placeholder='Enter Text Here' required />
+
+                    <label>Subject</label>
+                    <input class="input-field" type="text" name="subject" placeholder='Enter Text Here' required />
+
+                    <label>Email</label>
+                    <input class="input-field" type="email" name="email" placeholder='Enter Text Here' required />
+
+                    <label>Message</label>
+                    <textarea class="input-field" name="message" placeholder='Enter Text Here' ></textarea>
+
+                    <button onClick={submit}>Submit</button>
+                </form>
+
             </div>
         </div>
     )
