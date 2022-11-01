f5a019ce82f689f6c0cf120167dc71f5ae68713d
allanx1ing <allanxing2003@gmail.com>
2022-11-01 16:05:14 -0700

    added sorting algorithm project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,60 +1,56 @@
-import '../App.css';
-import Project from './Project'
-import ProjectsFile from '../data/projects.json'
-import { Link } from 'react-router-dom'
+import "../App.css"
+import Project from "./Project"
+import ProjectsFile from "../data/projects.json"
+import { Link } from "react-router-dom"
 
 export default function ProjectsPage() {
-
-    return (
-        <>
-            <div className='projects' id='projects'>
-                <h1>
-                    - Projects -
-                </h1>
-                <p>
-                    Click on a project to learn more!
-                </p>
-                {/* <Link href="/projects">
+  return (
+    <>
+      <div className="projects" id="projects">
+        <h1>- Projects -</h1>
+        <p>Click on a project to learn more!</p>
+        {/* <Link href="/projects">
 
                 </Link> */}
 
-                <span>
+        <span>
+          <span>
+            <a href="https://www.allanxing.com/sorting-visualizer/" className="projects-img" target='_blank'>
+              <img src="/sorting-visualizer.png" width={600} alt="sorting-visualizer" />
+            </a>
+            <p className="projects_name">
+              <a href="https://www.allanxing.com/sorting-visualizer/" target='_blank'>Sorting Visualizer</a>
+            </p>
+          </span>
 
-                    <span>
-                        <a href='/#/project3' className='projects-img'>
-                            <img src="/website-homescreen.png" width={600} />
-                        </a>
-                        <p className='projects_name'>
-                            <a href='/#/project3'>
-                                Personal Website
-                            </a>
-                        </p>
-                    </span>
+          <span>
+            <a href="/#/project3" className="projects-img">
+              <img src="/website-homescreen.png" width={600} />
+            </a>
+            <p className="projects_name">
+              <a href="/#/project3">Personal Website</a>
+            </p>
+          </span>
 
-                    <span>
-                        <a href='/#/project2' className='projects-img'>
-                            <img src="/Roblox1.png" width={600} />
-                        </a>
-                        <p className='projects_name'>
-                            <a href='/#/project2'>
-                                Game Development Contractor
-                            </a>
-                        </p>
-                    </span>
+          <span>
+            <a href="/#/project2" className="projects-img">
+              <img src="/Roblox1.png" width={600} />
+            </a>
+            <p className="projects_name">
+              <a href="/#/project2">Game Development Contractor</a>
+            </p>
+          </span>
 
-                    <span>
-                        <a href='/#/project1' className='projects-img'>
-                            <img src="/Roblox2.png" width={600} />
-                        </a>
-                        <p className='projects_name'>
-                            <a href='/#/project1'>
-                                Super Galaxy Jump Obby
-                            </a>
-                        </p>
-                    </span>
-
-                </span>
-            </div>
-        </>
-    )
+          <span>
+            <a href="/#/project1" className="projects-img">
+              <img src="/Roblox2.png" width={600} />
+            </a>
+            <p className="projects_name">
+              <a href="/#/project1">Super Galaxy Jump Obby</a>
+            </p>
+          </span>
+        </span>
+      </div>
+    </>
+  )
 }
