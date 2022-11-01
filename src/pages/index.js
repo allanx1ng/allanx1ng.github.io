58f7e44d2e48a2c9173a11ee57ca33a2132bed43
allanx1ing <allanxing2003@gmail.com>
2022-11-01 15:49:39 -0700

    formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,36 @@
-import logo from '../logo.svg';
-import '../App.css';
+import logo from "../logo.svg"
+import "../App.css"
 
-import Header from '../components/Header'
+import Header from "../components/Header"
 import Projects from "../components/Projects"
-import About from '../components/About'
-import Particles from '../components/Particles';
-import ScrollTop from '../components/ScrollTop'
-import Socials from '../components/Socials'
-import Skills from '../components/Skills'
-import Contact from '../components/Contact'
+import About from "../components/About"
+import Particles from "../components/Particles"
+import ScrollTop from "../components/ScrollTop"
+import Socials from "../components/Socials"
+import Skills from "../components/Skills"
+import Contact from "../components/Contact"
 
-import Typed from 'react-typed';
+import Typed from "react-typed"
 
-import { Icon } from '@iconify/react'
-import ScrollButton from '../components/ScrollButton';
+import { Icon } from "@iconify/react"
+import ScrollButton from "../components/ScrollButton"
 
 const HomePage = () => {
   return (
     <div>
-
       <Header />
-      <div className='body'>
-
-        <div className='intro' id='intro'>
-          <div className='nameText'>
-            Hello World, I{"'"}m{' '}
-            <div className='nameText_Name'>
-              Allan Xing
-            </div>
+      <div className="body">
+        <div className="intro" id="intro">
+          <div className="nameText">
+            Hello World, I{"'"}m <div className="nameText_Name">Allan Xing</div>
           </div>
 
-          <div className='animatedText'>
+          <div className="animatedText">
             <Typed
               strings={[
                 "Software Developer",
                 "UBC Student",
-                "Second Year CS Major"
+                "Second Year CS Major",
               ]}
               typeSpeed={80}
               backSpeed={50}
@@ -43,20 +38,19 @@ const HomePage = () => {
               backDelay={1500}
               loop
             />
-
           </div>
-          <ScrollButton style={'aboutMeButton'} text={'About Me '} destination={'about'} />
+          <ScrollButton
+            style={"aboutMeButton"}
+            text={"About Me "}
+            destination={"about"}
+          />
 
-          
           <Socials />
-
-
         </div>
 
-        <div className='pfp'>
-          <img src='/pfp.png'/>
+        <div className="pfp">
+          <img src="/pfp.png" />
         </div>
-
       </div>
       <ScrollTop icon={"ep:arrow-up-bold"} width={30} height={30} />
 
@@ -68,11 +62,9 @@ const HomePage = () => {
 
       <Contact />
 
-      <Particles className='particles' />
-
+      <Particles className="particles" />
     </div>
-
   )
 }
 
-export default HomePage;
+export default HomePage
